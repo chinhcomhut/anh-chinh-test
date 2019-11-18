4397c1276bd758a43a8013b974aa5ce9549df1bb
- <chinhcomhut@gmail.com>
2019-11-18 10:46:25 +0700

    bai thi cua Thao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,61 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BooksComponent } from './components/books/books.component';
+import { BookListComponent } from './components/books/book-list/book-list.component';
+import { BookAddComponent } from './components/books/book-add/book-add.component';
+import { BookEditComponent } from './components/books/book-edit/book-edit.component';
+import { BooksService} from './services/books.service';
+import {FormsModule} from "@angular/forms";
 
+const appRoutes: Routes = [
+  {
+    path : '',
+    component : HomeComponent
+  },
+  {
+    path : 'books',
+    component : BooksComponent,
+    children : [
+      {
+        path : '',
+        component : BookListComponent
+      },
+      {
+        path : ':id/edit',
+        component : BookEditComponent
+      },
+      {
+        path : 'add',
+        component : BookAddComponent
+      }
+    ]
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    BookListComponent,
+    BookAddComponent,
+    BookEditComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    BooksService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
